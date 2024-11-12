0c99b25dc4bc996c219a44e8d4e483cddbb2ca7b
Federico Miras <miras.federico@gmail.com>
2024-11-12 02:10:33 -0300

    create api endpoint

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,15 +1,32 @@
 import { openai } from '@ai-sdk/openai'
-import { streamText, convertToCoreMessages } from 'ai'
+import { streamText } from 'ai'
+import { fetchReviews } from '@/lib/steam'
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 60
-
+// curl -X POST http://localhost:3000/api/summarize -H "Content-Type: application/json" -d '{"gameId":"1091500"}'
 export async function POST(req: Request) {
-  const { messages } = await req.json()
+  const { gameId } = await req.json()
 
+  // Fetch reviews first
+  let { reviews, cursor } = await fetchReviews(gameId)
+
+  // Fetch up to 200 reviews
+  while (reviews.length < 200 && cursor) {
+    const data = await fetchReviews(gameId, cursor)
+    reviews.push(...data.reviews)
+    cursor = data.cursor
+  }
+
+  // Create the streaming response
   const result = await streamText({
-    model: openai('gpt-4o'),
-    messages: convertToCoreMessages(messages)
+    model: openai('gpt-4o-mini'),
+    messages: [
+      {
+        role: 'user',
+        content: `give me a summary for positive, negative and neutral separated by new lines, reviews are: ${reviews
+          .map((r) => r.review)
+          .join('\n')}`,
+      },
+    ],
   })
 
   return result.toDataStreamResponse()
